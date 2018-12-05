11b63cfac4f47026c89758ff8be3540bff003368
PaulCombal <abonnementspaul@gmail.com>
2018-12-05 16:29:24 +0100

    Setup blank project

---
+++ CookingPapa/CookingPapa/ModelTest/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace ModelTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
