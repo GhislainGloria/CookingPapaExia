ec9f9ecdd96ada6640c16c81393e0ac569d8b559
PaulCombal <abonnementspaul@gmail.com>
2018-12-06 13:57:44 +0100

    stash

--- CookingPapa/CookingPapa/Main/Main.cs
+++ CookingPapa/CookingPapa/Main/Main.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using View;
+using System.Windows.Forms;
+
 namespace Main
 {
     public class MainClass
     {
 		public static void Main()
         {
-			
+			MainWindow window = new MainWindow();         
+			Application.Run(window);
         }
     }
 }
