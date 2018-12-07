6d470bcf587c2d863558d672c57903422a394ca3
PaulCombal <abonnementspaul@gmail.com>
2018-12-07 09:47:41 +0100

    Added tests

---
+++ CookingPapa/CookingPapa/ViewTest/TextureFactoryTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using System;
+using View;
+
+namespace ViewTest
+{
+    [TestFixture()]
+    public class TextureFactoryTest
+    {
+        [Test()]
+        public void TestFactory()
+        {         
+			Assert.IsInstanceOf(typeof(TextureBrush), TextureFactory.CreateBrush("je n'existe pas"));
+			Assert.IsInstanceOf(typeof(TextureBrush), TextureFactory.CreateBrush("tile"));
+        }
+    }
+}
