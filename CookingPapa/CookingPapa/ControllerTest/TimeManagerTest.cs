5c5069825816dcb59591d01b4308ce674bdcfc76
PaulCombal <abonnementspaul@gmail.com>
2018-12-07 11:31:29 +0100

    Implemented TimeManager

---
+++ CookingPapa/CookingPapa/ControllerTest/TimeManagerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Controller;
+
+
+namespace ControllerTest
+{
+    [TestFixture()]
+    class TimeManagerTest
+    {
+
+        private readonly TimeManager timemanager = new TimeManager();
+        
+		private void fail()
+		{
+			Assert.Fail("Called at wrong moment");
+		}
+
+		private void success()
+		{
+			Assert.Pass("Test passed");
+		}
+
+        [Test()]
+        public void TestAll()
+        {
+			timemanager.NextMinute += fail;
+
+			for (int i = 0; i < 59; i++)
+			{
+				timemanager.Forward();
+			}
+
+			timemanager.NextMinute -= fail;         
+			timemanager.NextMinute += success;
+
+			timemanager.Forward(); // 60 seconds passed
+
+			timemanager.NextMinute -= success;         
+        }            
+    }
+}
