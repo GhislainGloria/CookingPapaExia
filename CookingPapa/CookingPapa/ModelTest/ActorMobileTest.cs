342b98c040bfc63a2c5bad3f6f1f4ec6f8410a2c
ghislain gloria <ghislain.gloria@viacesi.fr>
2018-12-07 13:08:34 +0100

    remove scrap again

--- CookingPapa/CookingPapa/ModelTest/ActorMobileTest.cs
+++ CookingPapa/CookingPapa/ModelTest/ActorMobileTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace ModelTest
 {
-    class ActorMobile
+    class ActorMobileTest
     {
+        ActorMobile actorMobile;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            actorMobile = new ActorMobile();
+        }
+        
+
+        [Test()]
+        public void CallStrategyTest()
+        {
+            Assert.actorMobile.CallStrategy();
+        }
+
+        public void NextTickTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+
+        }
     }
 }
--- CookingPapa/CookingPapa/ModelTest/ActorStaticTest.cs
+++ CookingPapa/CookingPapa/ModelTest/ActorStaticTest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ModelTest
 {
-    class ActorStatic
+    class ActorStaticTest
     {
     }
 }
