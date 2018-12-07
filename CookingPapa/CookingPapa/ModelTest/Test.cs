92ee9c0bb014b21a11c76421f51247b7c8646ee7
ArnaudOillic <arnaud.oillic@gmail.com>
2018-12-07 11:34:00 +0100

    avancees

--- CookingPapa/CookingPapa/ModelTest/Test.cs
+++ CookingPapa/CookingPapa/ModelTest/Test.cs
@@ -1,13 +1,1 @@
-﻿using NUnit.Framework;
-using System;
-namespace ModelTest
-{
-    [TestFixture()]
-    public class Test
-    {
-        [Test()]
-        public void TestCase()
-        {
-        }
-    }
-}
+﻿
