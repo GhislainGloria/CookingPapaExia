4b394cac27b5430b4a8e17d7c0c44553af64e3bd
ghislain gloria <ghislain.gloria@viacesi.fr>
2018-12-10 19:05:39 +0100

    StrategyFurnace :(

--- CookingPapa/CookingPapa/Model/StrategyFurnace.cs
+++ CookingPapa/CookingPapa/Model/StrategyFurnace.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class StrategyFurnace : Strategy
+    public class StrategyFurnace : Strategy
     {
         private static StrategyFurnace Instance = new StrategyFurnace();
         public static StrategyFurnace GetInstance()
@@ -21,68 +21,47 @@ namespace Model
 
         private void InitFurnace(IActor furnace, List<IActor> all)
         {
-            List<IActor> allCounters = all.Where(a => a.Name == "counter").ToList();
-            foreach (IActor a in allCounters)
-            {
-                a.EventGeneric += furnace.StrategyCallback;
-            }
             furnace.Initialized = true;
+            Console.WriteLine("Furnace Init");
         }
 
         public override void Behavior(IActor self, List<IActor> all)
         {
             if (!self.Initialized) InitFurnace(self, all);
 
-            if (self.Stack.Count > 0)
-            {
-                Console.WriteLine("Chef: I must complete {0} more orders.", self.Stack.Count);
-                Order topOrder = (Order)self.Stack[0];
-                List<IActor> partyLeaders = all.Where(a => a.Name == "partyleader").ToList();
+        //Item contient les ingredients
+        //Stack contient les etapes
 
-                if (topOrder.Completed())
+            if (self.Items.Count == 1 && self.Stack.Count == 1)
+            {
+                Ingredient ingredient = (Ingredient)self.Items[0];
+                Step step = (Step)self.Stack[0];
+                
+                if (step.TimeSpentSoFar == 0)
                 {
-                    Console.WriteLine("Chef has completed an order");
-                    // Fire an event or something
-                    self.Stack.RemoveAt(0);
+                    self.Busy = true;
+                    Console.WriteLine("Furnace On");
                 }
-                else
+                step.TimeSpentSoFar++;
+                if (step.TimeSpentSoFar >= step.TimeNeed)
                 {
-                    // We look for non-completed step, and assign it to a party leader
-                    foreach (Dish d in topOrder.DishInstances.ToList())
-                    {
-                        foreach (Step s in d.Steps.ToList())
-                        {
-                            if (!s.Prepared)
-                            {
-                                foreach (IActor a in partyLeaders)
-                                {
-                                    if (!a.Busy)
-                                    {
-                                        a.Stack.Add(s);
-                                        a.Busy = true;
-                                        Console.WriteLine("Chef has dispatched a step");
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    self.Busy = false;
+                    Console.WriteLine("Furnace Off");
+
+                    self.TriggerEvent("Dring", all);
+
+                    self.Items.Clear();
+                    self.Stack.Clear();
+                }
+
+                
                     // end foreach nest
                 }
             }
-        }
 
         public override void ReactToEvent(IActor self, MyEventArgs args)
         {
-            switch (args.EventName)
-            {
-                case "order received":
-                    // Ici on ajoute la nouvelle Order (avec args.Arg) dans la liste
-                    // des trucs à faire du chef. Le process se fera par la suite
-                    // dans Behavior()
-                    self.Stack.Add(args.Arg);
-                    break;
-            }
+            throw new NotImplementedException();
         }
     }
 }
---
+++ CookingPapa/CookingPapa/ModelTest/StrategyFurnaceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using NUnit.Framework;
+
+namespace ModelTest
+{
+    class StrategyFurnaceTest
+    {
+        StrategyFurnace strategyFurnace;
+        List<IActor> actors;
+        IActor Furnace;
+        private Ingredient ingredient;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            strategyFurnace = StrategyFurnace.GetInstance();
+          
+            List<IActor> actors = new List<IActor>();
+            ingredient = new Ingredient("beef", 2, 5);
+        }
+
+        [Test()]
+        public void BehaviorTest()
+        {
+                       
+                 strategyFurnace.Behavior(Furnace, actors);
+                 if (Furnace.Items.Contains(ingredient))
+                     {
+                         Assert.IsTrue(Furnace.Busy);
+                     }
+                     if (Furnace.Busy == true)
+                     {
+                         Assert.AreSame(Furnace.Items, ingredient);
+                     }
+                     if (Furnace.Stack.Count > 0)
+                     {
+                         Assert.IsTrue(Furnace.Busy);
+                     }
+                     if (Furnace.Stack.Count == 0)
+                     {
+                         Assert.IsFalse(Furnace.Busy);
+                     }
+             
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+
+        }
+
+    }
+
+}
